89800741f8a9f246d1a1596dd1851b16fb0e6ebc
tung17122k <sanking1712@gmail.com>
2025-07-02 01:17:12 +0700

    route server nextjs call to be

--- src/components/main/main.slider.tsx
+++ src/components/main/main.slider.tsx
@@ -1,25 +1,41 @@
 'use client'
 
-import { Badge, Box, Card, CardContent, CardMedia, Grid, IconButton, Typography } from "@mui/material";
+import { Badge, Box, Button, Card, CardContent, CardMedia, Grid, IconButton, Modal, Slide, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
+import { useState } from "react";
+import ModalMenu from "../modal/modal.menu";
 
 
 interface IProps {
     data: IProductCategory[],
-    title: String
+    title: string
 }
 
 const MainSlider = (props: IProps) => {
-    console.log("check data", props.data);
-    const data = props?.data
-    const title = props?.title
+    // console.log("check data", props.data);
+    const { data, title } = props
+    const [modalOpen, setModalOpen] = useState<boolean>(false)
+    const [item, setItem] = useState<IProductCategory | undefined>()
+
+    const handleOpenModal = () => {
+        setModalOpen(true)
+    }
+    const handleCloseModal = () => {
+        setModalOpen(false)
+    }
+
+    const handleClickItem = (item: IProductCategory) => {
+        setItem(item);
+        handleOpenModal();
+    };
+
     return (
         <div>
             <h2 >{title}</h2>
             <Grid container rowSpacing={2} columnSpacing={{ xs: 2, sm: 2, md: 3 }} >
                 {data.map(item => {
                     return (
-                        <Grid item xs={6} md={4} >
+                        <Grid item xs={6} md={4} key={item.id}>
                             <Card sx={{ position: 'relative', borderRadius: 2 }}>
                                 <Badge
                                     badgeContent={1}
@@ -31,6 +47,7 @@ const MainSlider = (props: IProps) => {
                                     height="100"
                                     image={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/product/${item?.image}`}
                                     alt={item.name}
+                                    onClick={() => handleClickItem(item)}
                                 />
                                 <CardContent sx={{ p: 1 }}>
                                     <Typography variant="body2" fontWeight="bold" noWrap>
@@ -40,17 +57,19 @@ const MainSlider = (props: IProps) => {
                                         <Typography variant="body2" color="error">
                                             {item?.price.toLocaleString('vi-VN') + 'đ'}
                                         </Typography>
-                                        <IconButton size="small" sx={{ backgroundColor: '#ff6600', color: 'white' }}>
+                                        <IconButton size="small" sx={{ backgroundColor: '#ff6600', color: 'white' }} >
                                             <AddIcon fontSize="small" />
                                         </IconButton>
                                     </Box>
                                 </CardContent>
                             </Card>
                         </Grid>
-
                     )
                 })}
             </Grid>
+            {
+                modalOpen && <ModalMenu item={item} modalOpen={modalOpen} handleCloseModal={handleCloseModal} />
+            }
         </div>
     )
 }
---
+++ src/components/modal/modal.menu.tsx
@@ -0,0 +1,57 @@
+import { Box, Button, Modal, Slide, Typography } from "@mui/material"
+
+interface IProps {
+    modalOpen: boolean,
+    handleCloseModal: () => void,
+    item?: IProductCategory
+}
+
+
+const ModalMenu = (props: IProps) => {
+    const { item, modalOpen, handleCloseModal } = props
+
+    return (
+        <div>
+            <Modal open={modalOpen} >
+                <Slide direction="up" in={modalOpen} mountOnEnter unmountOnExit>
+                    <Box
+                        sx={{
+                            position: 'fixed',
+                            bottom: 0,
+                            width: '100%',
+                            maxHeight: '90%',
+                            bgcolor: 'background.paper',
+                            borderRadius: '16px 16px 0 0',
+                            p: 2,
+                            overflowY: 'auto',
+                        }}
+                    >
+                        <Box
+                            component="img"
+                            src={`http://localhost:3000/api?image=${item?.image}`}
+                            alt="food"
+                            sx={{
+                                width: '100%',
+                                borderRadius: 2,
+                                mb: 2,
+                                objectFit: 'cover',
+                                height: 180,
+                            }}
+                        />
+                        <Typography variant="h6">{item?.name}</Typography>
+                        <Typography color="error" fontWeight="bold">{item?.price.toLocaleString('vi-VN') + 'đ'}</Typography>
+                        <Typography mt={1} variant="body2" color="text.secondary">
+                            {item?.description}
+                        </Typography>
+                        <Box mt={2} display="flex" justifyContent="space-between">
+                            <Button variant="outlined" onClick={() => handleCloseModal()}>Đóng</Button>
+                            <Button variant="contained" color="warning">Thêm vào giỏ hàng</Button>
+                        </Box>
+                    </Box>
+                </Slide>
+            </Modal>
+        </div>
+
+    )
+}
+export default ModalMenu
